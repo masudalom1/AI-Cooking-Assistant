bbcfc2372b8a8b7266e02ae087e2c44f72cd3cb5
masudalom1 <masudalom4322@gmail.com>
2025-08-07 15:07:18 +0530

    change in api

--- routes/ai.route.js
+++ routes/ai.route.js
@@ -48,7 +48,7 @@ router.post("/", protectRoute, async (req, res) => {
       success: true,
       recipe: recipeJSON,
       imageUrl: recipeJSON.imageUrl || null,
-      
+      _id:recipeJSON._id
     });
   } catch (err) {
     console.error("Recipe Generation Error:", err.message);
